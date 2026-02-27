b354cf1e4c3214ef7727b20965211f82985fa280
Florian Braun <Braunf25@proton.me>
2026-02-27 12:37:16 +0100

    Loading the backend

--- Eventify-Back-End/src/main/java/be/ucll/fs/project/unit/model/Event.java
+++ Eventify-Back-End/src/main/java/be/ucll/fs/project/unit/model/Event.java
@@ -15,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -53,7 +54,7 @@ public class Event {
     @OneToOne(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
     private EventDescription eventDescription;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
         name = "event_venues",
         joinColumns = @JoinColumn(name = "event_id"),
